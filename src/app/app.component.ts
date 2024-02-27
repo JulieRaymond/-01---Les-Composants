a387729ce359a2fec423534594072e08e6918244
Julie <raymondjulie@proton.me>
2024-02-27 17:06:34 +0100

    "exercice done"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BlockComponent } from "./block/block.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, BlockComponent]
 })
 export class AppComponent {
   title = 'myFirstComposant';
